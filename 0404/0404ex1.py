c94bdc9aa262c0661febac2c85b625a88e9fdb20
Nahyun <127096669+Na123hyun@users.noreply.github.com>
2023-04-04 13:16:07 +0900

    Add files via upload

---
+++ 0404/0404ex1.py
@@ -0,0 +1,18 @@
+'''
+2023.04.04
+김나현
+표준출력(print)연습
+'''
+
+print('한국','korea',2023)
+print('한국','korea',2023,sep='-') #구분자(-)에 넣어서 출력
+print('한국','korea',2023,end='@') #문장끝 @출력
+print("\n") #한 줄 띄우기
+print('python','languge');print("Hi python")
+print('python','languge',end=' ');print("Hi python") #줄 바꿈 없이 출력
+print("Hi pyton")
+print("'Hi pyton'") #작은 따옴표 출력
+print("\'Hi pyton\'") #작은 따옴표 출력
+print("pyton"*5) #5번 반복 출력
+print("hi \n pyton") #/n(줄바꿈) 출력
+print("\n 포맷 활용") #한줄 띄우고 문자열 출력
