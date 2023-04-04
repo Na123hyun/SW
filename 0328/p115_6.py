265b5523260590d879a314b9ec37b1e309a19fcd
Nahyun <127096669+Na123hyun@users.noreply.github.com>
2023-04-04 13:15:23 +0900

    Add files via upload

---
+++ 0328/p115_6.py
@@ -0,0 +1,21 @@
+'''
+2023.03.28
+김나현
+47과 18의 가감승제 구하는 프로그램
+'''
+
+su1=25 #정수
+su2=5 #정수
+
+res1=su1+su2 #res1 변수에 su1+su2 결과 저장
+res2=su1-su2 #res2 변수에 su1-su2 결과 저장
+res3=su1*su2 #res3 변수에 su1*su2 결과 저장
+res4=su1/su2 #res4 변수에 su1/su2 결과 저장
+
+print(su1,"과",su2,"의 합은 :",su1+su2)
+print(su1,"과",su2,"의 차은 :",su1-su2)
+print(su1,"과",su2,"의 곱은 :",su1*su2)
+print(su1,"과",su2,"의 나눗셈은 :",su1/su2)
+
+
+
