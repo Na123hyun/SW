c6559a6b4fa01800eb2a5cef49d6f0cec18a4e0c
Nahyun <127096669+Na123hyun@users.noreply.github.com>
2023-04-11 18:05:24 +0900

    Add files via upload

--- 0411/0411cal.py
+++ 0411/0411cal.py
@@ -2,13 +2,20 @@
 2023-04-11
 김나현
 숫자와 연산자를 정수로 입력 받아서 합계 구하기
-numder1(5),numder(2),op(+,-,*,/)
+num1(5),num(2),op(+,-,*,/)
 '''
 
-numder1=int(input("첫번째 정수를 입력하시오."))
-numder2=int(input("2"))
-op=(input("+,-,*,/"))
+num1=int(input("첫 번째의 숫자를 입력하시오."))
+num2=int(input("두 번째의 숫자를 입력하시오."))
+op=(input("+,-,*,/ 중에서 고르시오."))
 
 if op=="+":
-    print('결과값은',numder1+numder2)
-
+    print('결과 값은',num1+num2)
+elif op=="-":
+    print('결과 값은',num1-num2)
+elif op=="*":
+    print('결과 값은',num1*num2)
+elif op=="/":
+    print('결과 값은',num1/num2)
+else:
+    print("해당 연산자는 없습니다.")
